fe14c9672d083821d44a9b14086d9234c6f71993
Andrzej <andrzejlud@op.pl>
2014-09-11 20:44:02 +0200

    some test

---
+++ src/test/java/info/ludwikowski/fluentbuilder/sample/ClassWithoutDefaultConstructorTest.java
@@ -0,0 +1,44 @@
+/*
+ * Created on 11 wrz 2014 20:32:59 by Andrzej Ludwikowski
+ */
+
+package info.ludwikowski.fluentbuilder.sample;
+
+import static info.ludwikowski.fluentbuilder.sample.ClassWithoutDefaultConstructorBuilder.aClassWithoutDefaultConstructor;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+
+
+public class ClassWithoutDefaultConstructorTest {
+
+	@Test
+	public void shouldCreateObjectWithNotDefaultConstructor() {
+
+		// given
+		Integer number = 1;
+
+		// when
+		ClassWithoutDefaultConstructor classWithoutDefaultConstructor = aClassWithoutDefaultConstructor(number).build();
+
+		// then
+		assertThat(classWithoutDefaultConstructor.getNumber()).isEqualTo(number);
+	}
+
+	@Test
+	public void shouldCreateObjectWithAnotherNotDefaultConstructor() {
+
+		// given
+		Integer number = 1;
+		String someString = "someString";
+
+		// when
+		ClassWithoutDefaultConstructor classWithoutDefaultConstructor = aClassWithoutDefaultConstructor(number, someString).build();
+
+		// then
+		assertThat(classWithoutDefaultConstructor.getNumber()).isEqualTo(number);
+		assertThat(classWithoutDefaultConstructor.getSomeString()).isEqualTo(someString);
+	}
+
+}
